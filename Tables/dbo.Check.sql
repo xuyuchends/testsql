00c58604bb532ed9239919670fa979b075722ba2
xuyuchends <xuyuchends@gmail.com>
2015-08-11 12:39:31 +0800

    dd
    ff

--- Tables/dbo.Check.sql
+++ Tables/dbo.Check.sql
@@ -10,6 +10,7 @@ CREATE TABLE [dbo].[Check]
 [BusinessDate] [datetime] NOT NULL,
 [Status] [nvarchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [LastUpdate] [datetime] NOT NULL
+[LastUpdate1] [datetime]
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Check] ADD CONSTRAINT [PK_Check] PRIMARY KEY CLUSTERED  ([StoreID], [ID], [BusinessDate]) ON [PRIMARY]
